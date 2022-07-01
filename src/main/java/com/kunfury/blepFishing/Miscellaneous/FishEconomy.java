3e1b91be344d38463dbdae094642213232de596f
Kunfury-blep <65051061+Kunfury-blep@users.noreply.github.com>
2022-07-01 13:25:03 -0400

    Vastly improved efficiency of selling fish so that lag is now heavily reduced.

--- src/main/java/com/kunfury/blepFishing/Miscellaneous/FishEconomy.java
+++ src/main/java/com/kunfury/blepFishing/Miscellaneous/FishEconomy.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Objects;
 
 import io.github.bananapuncher714.nbteditor.NBTEditor;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -37,7 +38,6 @@ public class FishEconomy {
 			Economy econ = Setup.getEconomy();
 			if(econ != null){
 				List<ItemStack> itemList = new ArrayList<>();
-
 				if(sellAll) { //Runs if the player is wanting to sell all fish
 					for (ItemStack item : player.getInventory().getContents()) {
 						if(item != null && item.getType() == Material.SALMON) {
@@ -49,31 +49,43 @@ public class FishEconomy {
 
 				double total = 0;
 
-
+				//Adds the cost of all the fish together.
 				for (ItemStack item : itemList)
 				{
 
 					double value = NBTEditor.getDouble( item, "blep", "item", "fishValue" );
 					value *= priceMod;
-
-					if(value > 0) {
-						EconomyResponse r = econ.depositPlayer(player, value);
-						if(r.transactionSuccess()) {
-							if(itemList.size() <= 1) //Only sends out a message if a single fish is sold
-								player.sendMessage(ChatColor.translateAlternateColorCodes('&',
-										"Sold " + Objects.requireNonNull(item.getItemMeta()).getDisplayName()
-												+ String.format("&f for &a%s", econ.format(r.amount))));
-							item.setAmount(item.getAmount() - 1);
-							total += value;
-						} else {
-							player.sendMessage(String.format("An error occured: %s", r.errorMessage));
-						}
-					}
+					total += value;
 				}
-				if(itemList.size() > 1)
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&',
-							Variables.Prefix + "Sold " + itemList.size() + " fish"
-									+ "&f for &a" + Variables.CurrSym + Formatting.DoubleFormat(total) + "." ));
+
+				if(total > 0) {
+					EconomyResponse r = econ.depositPlayer(player, total);
+					if(r.transactionSuccess()) {
+						if(itemList.size() == 1){
+							ItemStack item = itemList.get(0);
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+									"Sold " + Objects.requireNonNull(item.getItemMeta()).getDisplayName()
+											+ String.format("&f for &a%s", econ.format(r.amount))));
+							item.setAmount(item.getAmount() - 1);
+						}else{
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+									Variables.Prefix + "Sold " + itemList.size() + " fish"
+											+ "&f for " + ChatColor.GREEN +Variables.CurrSym + Setup.getEconomy().format(total) + "." ));
+							for(var i : itemList){
+								i.setAmount(0);
+							}
+						}
+					} else {
+						player.sendMessage(String.format("An error occured: %s", r.errorMessage));
+					}
+				}else player.sendMessage(Variables.Prefix + "Those fish aren't worth anything.");
+
+
+
+
+
+
+
 			}
 		}
 
