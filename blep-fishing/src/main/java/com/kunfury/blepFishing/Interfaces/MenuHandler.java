cb0e49a7a86806b4714613226f94b9378cf451c4
Kunfury-blep <65051061+Kunfury-blep@users.noreply.github.com>
2023-04-28 16:57:16 -0400

    Improved Menus

--- blep-fishing/src/main/java/com/kunfury/blepFishing/Interfaces/MenuHandler.java
+++ blep-fishing/src/main/java/com/kunfury/blepFishing/Interfaces/MenuHandler.java
@@ -2,9 +2,12 @@ package com.kunfury.blepFishing.Interfaces;
 
 import com.kunfury.blepFishing.Commands.SubCommand;
 import com.kunfury.blepFishing.Commands.SubCommands.*;
+import com.kunfury.blepFishing.Conversations.ConversationHandler;
+import com.kunfury.blepFishing.Interfaces.Admin.AdminMenu;
 import com.kunfury.blepFishing.Interfaces.MenuButtons.*;
 import io.github.bananapuncher714.nbteditor.NBTEditor;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -28,23 +31,34 @@ public class MenuHandler implements Listener {
         menuButtons.add(new AdminTourneyButton());
         menuButtons.add(new AdminQuestButton());
         menuButtons.add(new AdminQuestMenuButton());
+        menuButtons.add(new AdminRarityMenuButton());
+        menuButtons.add(new AdminRarityButton());
     }
 
     @EventHandler
     public void inventoryClick(InventoryClickEvent e){
         ItemStack item = e.getCurrentItem();
-
         if(item != null && NBTEditor.contains(item, "blep", "item", "buttonId")){
+            e.setCancelled(true);
             String buttonId = NBTEditor.getString(item, "blep", "item", "buttonId");
 
             if(buttonId.equals("_background")){
-                e.setCancelled(true);
                 return;
+            }
+
+            switch (buttonId) {
+                case "adminRarityCreate" -> {
+                    ConversationHandler.StartRarityConversation((Player) e.getWhoClicked(), null);
+                    return;
+                }
+                case "adminRarityBack" -> {
+                    new AdminMenu().ShowMenu(e.getWhoClicked());
+                    return;
+                }
             }
 
             for(MenuButton menuButton : menuButtons){
                 if(menuButton.getId().equals(buttonId)){
-                    e.setCancelled(true);
                     menuButton.perform(e);
                     break;
                 }
@@ -66,6 +80,27 @@ public class MenuHandler implements Listener {
         return backgroundItem;
     }
 
+    public static ItemStack getBackButton(String buttonId){
+        ItemStack backButton = new ItemStack(Material.BARRIER, 1);
+        ItemMeta meta  = backButton.getItemMeta();
+        assert meta != null;
+        meta.setDisplayName(ChatColor.RED + "Go Back");
+        backButton.setItemMeta(meta);
+
+        backButton = NBTEditor.set(backButton, buttonId,"blep", "item", "buttonId");
+        return backButton;
+    }
+
+    public static ItemStack getCreateButton(String buttonId){
+        ItemStack createButton = new ItemStack(Material.EMERALD_BLOCK, 1);
+        ItemMeta meta  = createButton.getItemMeta();
+        assert meta != null;
+        meta.setDisplayName(ChatColor.GREEN + "Create New");
+        createButton.setItemMeta(meta);
+
+        createButton = NBTEditor.set(createButton, buttonId,"blep", "item", "buttonId");
+        return createButton;
+    }
 
 
 }
